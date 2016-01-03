fe3d0788f511c7b467da47b19a3d840cc185871a
phucngta <phucngta@gmail.com>
2016-01-04 01:14:43 +0700

    Thêm ajax hoàn chỉnh menu danh mục

--- application/views/admin/danhmuc_view/danhmuc_update_view.php
+++ application/views/admin/danhmuc_view/danhmuc_update_view.php
@@ -1,28 +1,24 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');?>
-<?php
-foreach($danhmuc as $dmc)
-{
-  // $madm= $this->uri->segment(4);
-  // if($madm== $dmc->madm)
-  // {
-  //   $madm= $dmc->madm;
-  //   $tendm= $dmc->tendm;
-  // }
-  echo $dmc->madm.'<div>'.$dmc->tendm.'</div>';
-}
-?>
+
 <div class="row">
   <div class="col-lg-7">
     <form action="<?php echo base_url().'admin/danhmuc/update'?>" method="post" name="suadanhmuc"  class="form-horizontal">
+      <?php
+      foreach($danhmuc as $dmc)
+        {?>
+
       <div class="form-group">
         <label class="control-label col-sm-3">Tên Danh Mục</label>
         <div class="col-sm-6">
-          <input type="text" value="" class="form-control" name="tendm" placeholder="" required>
+          <input type="text" value="<?php echo $dmc->tendm?>" class="form-control" name="tendm" placeholder="" required>
         </div>
         <div class="col-sm-3">
          <button type="submit" name="suadanhmuc" class="btn btn-primary">Cập nhập</button>
        </div>
-       <input type="hidden" name="madm" value="">
+       <input type="hidden" name="madm" value="<?php echo $dmc->madm?>">
      </form>
    </div>
+
+   <?php }
+   ?>
  </div>
